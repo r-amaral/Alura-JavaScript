68e4a0faa7f8ddf6a32654356c5ee8c9ca24930f
Ruan Oliveira <ruanamaral40@gmail.com>
2022-07-13 18:43:40 -0300

    Curso Finalizado

--- Cliente.js
+++ Cliente.js
@@ -1,13 +1,16 @@
 export class Cliente {
-    nome;
-    _cpf;
-
     get cpf() {
         return this._cpf;
     }
 
-    constructor(nome, cpf) {
+    constructor(nome, cpf, senha) {
         this.nome = nome;
         this._cpf = cpf;
+        this._senha = senha;
+        this.autenticar = 1;
+    }
+
+    autenticar() {
+        return true;
     }
 }
--- index.js
+++ index.js
@@ -17,15 +17,15 @@ import { Gerente } from "./funcionarios/Gerente";
 import { Diretor } from "./funcionarios/Diretor"
 import { SistemaAutenticacao } from "./SistemaDeAutenticacao.js";
 
-// const cliente1 = new Cliente("Ricardo", "11122233309");
-// const cliente2 = new Cliente("Alice", "88822233309");
+const cliente1 = new Cliente("Ricardo", "11122233309");
+const cliente2 = new Cliente("Alice", "88822233309");
 
-// const contaCorrenteRicardo = new ContaCorrente(0, cliente1, 1001);
-// const contaCorrenteAlice = new ContaPoupanca(0, cliente2, 1001);
+const contaCorrenteRicardo = new ContaCorrente(0, cliente1, 1001);
+const contaCorrenteAlice = new ContaPoupanca(0, cliente2, 1001);
 
-// contaCorrenteRicardo.depositar(300)
+contaCorrenteRicardo.depositar(300)
 
-// const ContaPoupancaRicardo = new Conta(50, cliente1, 1001);
+const ContaPoupancaRicardo = new Conta(50, cliente1, 1001);
 
 const diretor = new Diretor("Rodrigo", 10000, 112233445);
 const gerente = new Gerente("Rodrigo", 10000, 112233445);
@@ -33,5 +33,7 @@ const gerente = new Gerente("Rodrigo", 10000, 112233445);
 diretor.cadastrarSenha("123456")
 gerente.cadastrarSenha("123")
 
-const estaLogado = SistemaAutenticacao.login(diretor, '123456789');
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456789');
+
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123');
 
